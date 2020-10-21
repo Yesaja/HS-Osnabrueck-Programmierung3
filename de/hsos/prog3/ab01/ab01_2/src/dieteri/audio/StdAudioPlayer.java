c135a17bc018bee5fec5e70a128ebf2976eb7e81
Yesaja <steffwaschk@msn.com>
2020-10-21 21:16:41 +0200

    New structure for the whole repository: de\hsos\prog3\ followed by the actual tasks. User repos are stored in the src folder under tasks, thus using the same ressources (i.e. on lib).
    Changes can now be made "from the bottom up", first merging different user repos to one macro repo und finally the master.

---
+++ de/hsos/prog3/ab01/ab01_2/src/dieteri/audio/StdAudioPlayer.java
@@ -0,0 +1,15 @@
+package src.dieteri.audio;
+
+import java.io.IOException;
+import java.net.URL;
+
+public interface StdAudioPlayer {
+    //um die mit dem Übergabeparameter definierte wav‐Datei einmalig abzuspielen
+    void einmaligAbspeilen(URL url) throws IOException;
+    //Übergabeparameter legt fest, wie häufig der Player die definierte wav‐Datei wiederholtabspielt.
+    void wiederholtAbspeilen(URL url, int wiederholungen) throws IOException;
+
+    void tonAus();
+    void tonAn();
+
+}
