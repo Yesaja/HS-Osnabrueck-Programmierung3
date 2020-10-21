d1dccd73e45e3508c717ed5ecd13e065aaeefd0f
pjd <pjd-popos@pop-os.localdomain>
2020-10-21 12:12:43 +0200

    StdAudioPlayer Interface

--- ab01/ab01_2/src/de/hsos/prog3/ab1/audio/StdAudioPlayer.java
+++ ab01/ab01_2/src/de/hsos/prog3/ab1/audio/StdAudioPlayer.java
@@ -3,7 +3,7 @@ package de.hsos.prog3.ab1.audio;
 import java.io.IOException;
 import java.net.URL;
 
-public interface StdAutioPlayer{
+public interface StdAudioPlayer {
     //um die mit dem Übergabeparameter definierte wav‐Datei einmalig abzuspielen
     void einmaligAbspeichern(URL url) throws IOException;
     //Übergabeparameter legt fest, wie häufig der Player die definierte wav‐Datei wiederholtabspielt.
