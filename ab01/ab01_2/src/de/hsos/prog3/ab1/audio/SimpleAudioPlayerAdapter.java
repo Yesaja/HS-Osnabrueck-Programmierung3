183c1500ecee49eb66f46faf83219f920ba22e26
pjd <pjd-popos@pop-os.localdomain>
2020-10-21 12:21:49 +0200

    rechtschreib fehler fix

--- ab01/ab01_2/src/de/hsos/prog3/ab1/audio/SimpleAudioPlayerAdapter.java
+++ ab01/ab01_2/src/de/hsos/prog3/ab1/audio/SimpleAudioPlayerAdapter.java
@@ -5,18 +5,37 @@ import de.hsos.prog3.audio.SimpleAudioPlayer;
 import java.io.IOException;
 import java.net.URL;
 
-public class SimpleAudioPlayerAdapter implements  StdAutioPlayer{
-    SimpleAudioPlayer player;
+public class SimpleAudioPlayerAdapter implements StdAudioPlayer {
+    private SimpleAudioPlayer player;
     @Override
     public void einmaligAbspeichern(URL url) throws IOException {
-        player = new SimpleAudioPlayer(url);
-        player.setDebug(false);
-        player.verboseLogging(true);
+        if(player == null)
+            player = new SimpleAudioPlayer(url);
         player.play(0);
     }
 
     @Override
-    public void wiederholtAbspeilen(URL url, int wiederholungen) {
+    public void wiederholtAbspeilen(URL url, int wiederholungen) throws IOException {
+        for (int i = 0; i <= wiederholungen ; i++) {
+            einmaligAbspeichern(url);
+        }
+    }
 
+    @Override
+    public void tonAus() {
+        if(player != null){
+            player.setDebug(true);
+            player.verboseLogging(true);
+            System.out.println("Ton wurde Ausgeschaltet");
+        }
+    }
+
+    @Override
+    public void tonAn() {
+        if(player != null){
+            player.setDebug(false);
+            player.verboseLogging(true);
+            System.out.println("Ton wurde angeschaltet");
+        }
     }
 }
--- ab01/ab01_2/src/de/hsos/prog3/ab1/audio/StdAudioPlayer.java
+++ ab01/ab01_2/src/de/hsos/prog3/ab1/audio/StdAudioPlayer.java
@@ -5,7 +5,7 @@ import java.net.URL;
 
 public interface StdAudioPlayer {
     //um die mit dem Übergabeparameter definierte wav‐Datei einmalig abzuspielen
-    void einmaligAbspeichern(URL url) throws IOException;
+    void einmaligAbspeilen(URL url) throws IOException;
     //Übergabeparameter legt fest, wie häufig der Player die definierte wav‐Datei wiederholtabspielt.
     void wiederholtAbspeilen(URL url, int wiederholungen) throws IOException;
 
