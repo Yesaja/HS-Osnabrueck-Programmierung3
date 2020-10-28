4afa6ec485bf10e22262276592b5d7d2cff68024
pjd <pjd-popos@pop-os.localdomain>
2020-10-28 11:38:47 +0100

    create SpielfeldDarstellung template

---
+++ de/hsos/prog3/ab02/ui/SpielfeldDarstellung.java
@@ -0,0 +1,11 @@
+package ab02.ui;
+
+import ab02.util.Interaktionsbrett;
+
+class SpielfeldDarstellung {
+    private Interaktionsbrett ib;
+
+    SpielfeldDarstellung(Interaktionsbrett ib){}
+    SpielfeldDarstellung(boolean[][] spielfeld){}
+    public void abwischen(){};
+}
