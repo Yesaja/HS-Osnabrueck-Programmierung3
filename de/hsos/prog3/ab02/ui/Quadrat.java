37c4d1408f5bd155a8dd280910b75b2147bbcfd0
pjd <pjd-popos@pop-os.localdomain>
2020-10-28 11:30:38 +0100

    create Quadrat template

---
+++ de/hsos/prog3/ab02/ui/Quadrat.java
@@ -0,0 +1,21 @@
+package ab02.ui;
+
+import ab02.util.Interaktionsbrett;
+
+public class Quadrat {
+    
+    private int x; 
+    private int y; 
+    private int seitenlaenge;
+
+    Quadrat(){};
+
+    public void darstellenRahmen(Interaktionsbrett ib){
+
+    }
+    public void darstellenFuellung(Interaktionsbrett ib){
+
+    }
+
+
+}
