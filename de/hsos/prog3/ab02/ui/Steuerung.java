90bf9c66fb932f49c5a918fcd1967a8b117dd07c
pjd <pjd-popos@pop-os.localdomain>
2020-10-30 17:01:49 +0100

    implement Steuerung.java

--- de/hsos/prog3/ab02/ui/Steuerung.java
+++ de/hsos/prog3/ab02/ui/Steuerung.java
@@ -1,13 +1,37 @@
 package ui;
+import logik.Simulation;
+import logik.BeiAenderung;
+import util.EinUndAusgabe;
+import util.Interaktionsbrett;
 
-public class Steuerung {
-    
+public class Steuerung implements BeiAenderung{
+ 
+    private Simulation sim;
+    private NutzerEingabe eingabe;
+    private SpielfeldDarstellung darstellung;
 
-    public void startDesSpiels(){}
+    public void startDesSpiels(){
+        int schritt; 
+        initialisuerng();
+        int zellen = eingabe.anzahlZellenDesSpielfelds();
+        int probability = eingabe.wahrscheinlichkeitDerBesiedlung();
+        sim.berechneAnfangsGeneration(zellen, probability);
 
-    private void initialisuerng(){}
+        while(schritt > 0){
+            schritt = eingabe.anzahlDerSimulationsschitte();
+            sim.berechneAnfangsGeneration(schritt);
+        }
 
-    private void aktualisiere(boolean[][] neueGeneration){
-
+    }
+    public void initialisuerng(){
+        eingabe = new NutzerEingabe(new Interaktionsbrett());
+        darstellung = new SpielfeldDarstellung(new EinUndAusgabe());
+        sim.anmeldenFuerAktualisierungBeiAenderung(this);
+        
+    }
+    @Override
+    public  void aktualisiere(boolean[][] neueGeneration){
+        darstellung.abwischen();
+        darstellung.SpielfeldDarstellung(neueGeneration);
     }
 }
