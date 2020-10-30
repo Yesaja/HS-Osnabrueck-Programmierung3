8db3992467a8da67b8bcfbdc0d2848f9abf88b1a
pjd <pjd-popos@pop-os.localdomain>
2020-10-30 13:10:34 +0100

    implement SpielfledDarstellung.java

--- de/hsos/prog3/ab02/ui/SpielfeldDarstellung.java
+++ de/hsos/prog3/ab02/ui/SpielfeldDarstellung.java
@@ -4,8 +4,29 @@ import util.Interaktionsbrett;
 
 class SpielfeldDarstellung {
     private Interaktionsbrett ib;
-
-    SpielfeldDarstellung(Interaktionsbrett ib){}
-    SpielfeldDarstellung(boolean[][] spielfeld){}
-    public void abwischen(){};
+    private final int SEITENLAENGE_SPIELFELD = 300;
+    private final int MARGIN = 10;
+    
+    SpielfeldDarstellung(Interaktionsbrett ib){
+        this.ib = ib;
+    }
+    SpielfeldDarstellung(boolean[][] spielfeld){
+        int seitenlaenge = SEITENLAENGE_SPIELFELD / spielfeld.length;
+        int x = MARGIN; 
+        int y = MARGIN;
+        for(boolean[] a : spielfeld){
+            for(boolean b : a){
+                Quadrat quadrat = new Quadrat(x,y,seitenlaenge);
+                if(b) quadrat.darstellenFuellung(ib);
+                else quadrat.darstellenRahmen(ib);
+                
+                x += seitenlaenge;
+            }
+            x = MARGIN;
+            y += seitenlaenge;
+        }
+    }
+    public void abwischen(){
+        ib.abwischen();
+    };
 }
