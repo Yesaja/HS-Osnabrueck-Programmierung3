84cad82848920e945e895c834fc62dc51dfb382c
Yesaja <steffwaschk@msn.com>
2020-11-01 02:30:45 +0100

    method berechneAnfangsGeneration, not tested yet

--- de/hsos/prog3/ab02/logik/Simulator.java
+++ de/hsos/prog3/ab02/logik/Simulator.java
@@ -1,13 +1,24 @@
 package logik;
 
+import java.util.Random;
+
 public class Simulator implements Simulation {
-    private boolean[][] spielfeld;
+    private boolean[][] spielfeld; //bewohnt oder unbewohnt
     private int anzahlFelder;
 
     @Override
     public void berechneAnfangsGeneration(int anzahlDerZellen, int wahrscheinlichkeitDerBesiedlung) {
-        if()
-
+        this.anzahlFelder = anzahlDerZellen;
+        Random zufallszahl = new Random();
+        for(int zeile = 0; zeile < anzahlFelder / 2; zeile++) {
+            for(int spalte = 0; spalte < anzahlFelder / 2; spalte++) {
+                if(wahrscheinlichkeitDerBesiedlung > zufallszahl.nextInt(101)) {
+                    this.spielfeld[zeile][spalte] = true;
+                } else {
+                    this.spielfeld[zeile][spalte] = false;
+                }
+            }
+        }
     }
 
     @Override
