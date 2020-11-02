dea8cfb35d1b41ca231227f5a3de70a3b909be7a
Yesaja <steffwaschk@msn.com>
2020-11-02 20:01:45 +0100

    Readability + corrected methods + corrected parameters

--- de/hsos/prog3/ab02/ui/Steuerung.java
+++ de/hsos/prog3/ab02/ui/Steuerung.java
@@ -4,34 +4,36 @@ import logik.BeiAenderung;
 import util.EinUndAusgabe;
 import util.Interaktionsbrett;
 
-public class Steuerung implements BeiAenderung{
+public class Steuerung implements BeiAenderung {
  
     private Simulation sim;
     private NutzerEingabe eingabe;
     private SpielfeldDarstellung darstellung;
 
-    public void startDesSpiels(){
-        int schritt; 
-        initialisuerng();
+    public void startDesSpiels() {
+        int schritt = 0; 
+        initialisierung();
         int zellen = eingabe.anzahlZellenDesSpielfelds();
         int probability = eingabe.wahrscheinlichkeitDerBesiedlung();
         sim.berechneAnfangsGeneration(zellen, probability);
 
         while(schritt > 0){
             schritt = eingabe.anzahlDerSimulationsschitte();
-            sim.berechneAnfangsGeneration(schritt);
+            sim.berechneAnfangsGeneration(schritt, probability);
         }
 
     }
-    public void initialisuerng(){
-        eingabe = new NutzerEingabe(new Interaktionsbrett());
-        darstellung = new SpielfeldDarstellung(new EinUndAusgabe());
+
+    public void initialisierung() {
+        eingabe = new NutzerEingabe(new EinUndAusgabe());
+        darstellung = new SpielfeldDarstellung(new Interaktionsbrett());
         sim.anmeldenFuerAktualisierungBeiAenderung(this);
         
     }
+
     @Override
-    public  void aktualisiere(boolean[][] neueGeneration){
+    public void akualisiere(boolean[][] neueGeneration){
         darstellung.abwischen();
-        darstellung.SpielfeldDarstellung(neueGeneration);
+        darstellung.spielfeldDarstellen(neueGeneration);
     }
 }
