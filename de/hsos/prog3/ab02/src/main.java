e069adb09efa88b21697cd12f324e3e021106fdb
pjd <pjd-popos@pop-os.localdomain>
2020-11-03 17:31:55 +0100

    start a game

--- de/hsos/prog3/ab02/src/main.java
+++ de/hsos/prog3/ab02/src/main.java
@@ -1,7 +1,16 @@
 package src;
 
+import ui.Steuerung;
 public class main {
     
     public static void main(String[] args) {
+        Steuerung s = new Steuerung();
+        
+        try{
+            s.startDesSpiels();
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        
     }
 }
