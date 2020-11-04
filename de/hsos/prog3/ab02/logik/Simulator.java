b088b0a9af60f3be2bccca5236d87b352a7ba0e8
Yesaja <steffwaschk@msn.com>
2020-11-04 03:48:19 +0100

    berechneFolgeGeneration(int) finished

--- de/hsos/prog3/ab02/logik/Simulator.java
+++ de/hsos/prog3/ab02/logik/Simulator.java
@@ -28,18 +28,29 @@ public class Simulator implements Simulation {
     
     @Override
     public void berechneFolgeGeneration(int berechnungsschritte) throws InterruptedException { 
+        //Spielfeldgrenzen bestimmen
         int zeileLimit = spielfeld.length;
         int spalteLimit = spielfeld[0].length;
+        //So viele berechnen, wie der User wuenscht
         for(int i = 0; i < berechnungsschritte; i++) {
+            //Kopie des Spielfeldes anlegen
             boolean[][] spielbrett = new boolean[zeileLimit][spalteLimit];
             for(int zeile = 0; zeile < zeileLimit; zeile++) {
                 for(int spalte = 0; spalte < spalteLimit; spalte++) {
                     int anzahlNachbar = berechneNachbar(zeile,spalte);
 
-                    //regeln
-                   if(anzahlNachbar >= 3 && !spielfeld[zeile][spalte]){ spielbrett[zeile][spalte] = true;}
-                   if(anzahlNachbar < 2){ spielbrett[zeile][spalte] =  false;}
-                   if(anzahlNachbar < 3 ){ spielbrett[zeile][spalte] =  false; }                   
+                    //1. jede bewohnte Zelle mit genau {2,3} bewohnten Nachbarn = bleibt bewohnt
+                    //2. jede unbewohnte Zelle mit genau {3} bewohnten Nachbarn = wird bewohnt
+                    //3. jede Zelle hat [3,8] Nachbarn (min=Ecke, max=Mitte)
+                    
+                    if(anzahlNachbar == 2 || anzahlNachbar == 3) {
+                        spielbrett[zeile][spalte] = true;
+                    } else {
+                        spielbrett[zeile][spalte] = false;
+                    }
+                    /*if(spielfeld[zeile][spalte] && anzahlNachbar < 2 || anzahlNachbar > 3) {spielbrett[zeile][spalte] = false;}
+                    if(anzahlNachbar < 3 ) {spielbrett[zeile][spalte] = false;}                   
+                    if(!spielfeld[zeile][spalte] && anzahlNachbar == 3) {spielbrett[zeile][spalte] = true;}*/
                 }
             }
             this.spielfeld = spielbrett;
@@ -51,92 +62,33 @@ public class Simulator implements Simulation {
     private int berechneNachbar(int x, int y){
         int nachbar = 0;
         //oben
-        if(isNachbar(x-1, y-1)){nachbar++;}
-        if(isNachbar(x, y-1)){nachbar++;}
-        if(isNachbar(x+1, y-1)){nachbar++;}
+        if(istNachbar(x-1, y-1))    {nachbar++;}
+        if(istNachbar(x, y-1))      {nachbar++;}
+        if(istNachbar(x+1, y-1))    {nachbar++;}
         //mitte
-        if(isNachbar(x-1, y)){nachbar++;}
-        if(isNachbar(x+1, y)){nachbar++;}
+        if(istNachbar(x-1, y))      {nachbar++;}
+        if(istNachbar(x+1, y))      {nachbar++;}
         //unten
-        if(isNachbar(x-1, +-1)){nachbar++;}
-        if(isNachbar(x, y+1)){nachbar++;}
-        if(isNachbar(x+1, y+1)){nachbar++;}
+        if(istNachbar(x-1, +-1))    {nachbar++;}
+        if(istNachbar(x, y+1))      {nachbar++;}
+        if(istNachbar(x+1, y+1))    {nachbar++;}
         return nachbar;
     }
     
-    private boolean isNachbar(int x, int y){
+    private boolean istNachbar(int x, int y){
+        //innerhalb des Spielfeldes?
         if(x < 0 || y < 0 || x >= spielfeld.length || y >=spielfeld[x].length){
             return false;
         }
-        return spielfeld[x][y];
-    }
-        /*
-        public void berechneFolgeGeneration(int berechnungsschritte) {
-        for(int i = 0; i < berechnungsschritte; i++) {
-
-            //Grenzen des Spielfelds festhalten
-            int zeileLimit = spielfeld.length;
-            int spalteLimit = spielfeld[0].length;
-
-            for(int zeile = 0; zeile < zeileLimit-1; zeile++) {
-                for(int spalte = 0; spalte < spalteLimit-1; spalte++) {
-
-                    //Nachbarn der jeweiligen Zelle rausfinden
-                    int bewohnteNachbarn = 0;
-
-                    boolean obenLinks = spielfeld[zeile-1][spalte-1];
-                    boolean obenMitte = spielfeld[zeile-1][spalte];
-                    boolean obenRechts = spielfeld[zeile-1][spalte+1];
-
-                    boolean mitteLinks = spielfeld[zeile][spalte-1];
-                    boolean mitte = spielfeld[zeile][spalte]; //die Zelle selbst, weiter unten wichtig
-                    boolean mitteRechts = spielfeld[zeile][spalte+1];
-
-                    boolean untenLinks = spielfeld[zeile+1][spalte-1];
-                    boolean untenMitte = spielfeld[zeile+1][spalte];
-                    boolean untenRechts = spielfeld[zeile+1][spalte+1];
-
-                    if(zeile > 1){ 				        //Zeile drüber
-                        if(obenLinks) bewohnteNachbarn++;
-                        if(obenMitte) bewohnteNachbarn++;
-                        if(obenRechts) bewohnteNachbarn++;
-                    }
-                    if(zeile < zeileLimit - 1){			//Zeile drunter
-                        if(untenLinks) bewohnteNachbarn++; 
-                        if(untenMitte) bewohnteNachbarn++;
-                        if(untenRechts) bewohnteNachbarn++;
-                        
-                    }
-                    if(spalte > 1){				        //Spalte links
-                        if(obenLinks) bewohnteNachbarn++;
-                        if(mitteLinks) bewohnteNachbarn++;
-                        if(untenLinks) bewohnteNachbarn++;
-                    }
-                    if(spalte < spalteLimit - 1){		//Spalte rechts
-                        if(obenRechts) bewohnteNachbarn++;
-                        if(mitteRechts) bewohnteNachbarn++;
-                        if(untenRechts) bewohnteNachbarn++;
-                    }
-
-                    //Nachbarn rausgefunden, jetzt gucken, was wir damit machen
-
-                    //Regel 1.
-                    if(mitte == true && bewohnteNachbarn != 2 || bewohnteNachbarn != 3) {
-                        mitte = false;
-                    //Regel 2.
-                    } else if(mitte == false && bewohnteNachbarn == 3) {
-                        mitte = true;
-                    }
-                }
-            }
-        }
         
-       //1. jede bewohnte Zelle mit genau {2,3} bewohnten Nachbarn = bleibt bewohnt
-       //2. jede unbewohnte Zelle mit genau {3} bewohnten Nachbarn = wird bewohnt
-       //3. jede Zelle hat [3,8] Nachbarn (min=Ecke, max=Mitte)
+        //bewohnt oder nicht?
+        if(spielfeld[x][y]) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    */
-
+ 
     @Override
     public void anmeldenFuerAktualisierungBeiAenderung(BeiAenderung beiAenderung) {
         this.beiAenderung = beiAenderung;
