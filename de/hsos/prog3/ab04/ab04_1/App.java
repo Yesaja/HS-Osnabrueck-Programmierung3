e22c2c03f67dd90b1ebea7799df2530fb682f576
Yesaja <steffwaschk@msn.com>
2020-11-23 19:44:08 +0100

    timer added

--- de/hsos/prog3/ab04/ab04_1/App.java
+++ de/hsos/prog3/ab04/ab04_1/App.java
@@ -7,23 +7,26 @@ public class App {
         
         Interaktionsbrett spielfeld = new Interaktionsbrett();
 
+        long start, stop;
+
         Ball ball = new Ball(spielfeld);
         Korb korb = new Korb(spielfeld);        
 
         ball.darstellen(spielfeld);
         korb.darstellen(spielfeld);
 
-        spielfeld.starteUhr();
+        start = System.currentTimeMillis();
 
         while(true) {
             if(korb.getroffen(ball)) {
                 System.out.println("Treffer!");
+                stop = System.currentTimeMillis() - start;
+                spielfeld.neuerText(15,15,"Benötigte Zeit: " + stop);
+                return;
             } else {
                 try {
                     Thread.sleep(100);
-                } catch (Exception e) {
-    
-                }
+                } catch (Exception e) {}
             }
         }
     }
